9ab41dce3c658fb90485453671e3e31d59c324b8
shankarmuduli <50876848+shankarmuduli@users.noreply.github.com>
2019-09-28 11:23:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 
 
-// 			function countdown(){
-// 				var now = new Date();
-// 				var eventDate = new Date(2020, 03, 05);
+			function countdown(){
+				var now = new Date();
+				var eventDate = new Date(2020, 03, 05);
 
-// 				var currentTiime = now.getTime();
-// 				var eventTime = eventDate.getTime();
+				var currentTiime = now.getTime();
+				var eventTime = eventDate.getTime();
 
-// 				var remTime = eventTime - currentTiime;
+				var remTime = eventTime - currentTiime;
 
-// 				var s = Math.floor(remTime / 1000);
-// 				var m = Math.floor(s / 60);
-// 				var h = Math.floor(m / 60);
-// 				var d = Math.floor(h / 24);
+				var s = Math.floor(remTime / 1000);
+				var m = Math.floor(s / 60);
+				var h = Math.floor(m / 60);
+				var d = Math.floor(h / 24);
 
-// 				h %= 24;
-// 				m %= 60;
-// 				s %= 60;
+				h %= 24;
+				m %= 60;
+				s %= 60;
 
-// 				h = (h < 10) ? "0" + h : h;
-// 				m = (m < 10) ? "0" + m : m;
-// 				s = (s < 10) ? "0" + s : s;
+				h = (h < 10) ? "0" + h : h;
+				m = (m < 10) ? "0" + m : m;
+				s = (s < 10) ? "0" + s : s;
 
-// 				document.getElementById("days").textContent = d;
-// 				document.getElementById("days").innerText = d;
+				document.getElementById("days").textContent = d;
+				document.getElementById("days").innerText = d;
 
-// 				document.getElementById("hours").textContent = h;
-// 				document.getElementById("minutes").textContent = m;
-// 				document.getElementById("seconds").textContent = s;
+				document.getElementById("hours").textContent = h;
+				document.getElementById("minutes").textContent = m;
+				document.getElementById("seconds").textContent = s;
 
-// 				setTimeout(countdown, 1000);
-// 			}
+				setTimeout(countdown, 1000);
+			}
 
-//             countdown();
+            
             
             var slideIndex = 0;
       carousel();
